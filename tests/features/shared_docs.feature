406ece8a42ed8a6ea1f458ed8e0ae3ba283ac6a7
Buddy Arifin <buddy.arifin@bizzy.co.id>
2018-11-25 03:47:02 +0700

    page object models

--- tests/features/shared_docs.feature
+++
@@ -1,9 +0,0 @@
-Feature: Shared Docs
-
-  As I User
-  I want be able to input text into textfiled
-  So that other people able to see my notes
-
-  Scenario: Form Submission
-    Given I navigate to the Gist page
-    When I filled gist description
---
+++ tests/pages/gits_page.js
@@ -0,0 +1,14 @@
+class Gits {
+    desc = "input[name='gist[description]']";
+    filename = "input[name='gist[contents][][name]']";
+    indentMode = "select#indent-mode";
+    indentSize = "select#indent-size";
+    indentWrapMode = "select#indent-line-wrap-mode";
+    textFieldContainer = "div.CodeMirror-scroll";
+    textPerLine = "span[role='presentation']";
+
+
+    inputDesc() {
+
+    }
+}
